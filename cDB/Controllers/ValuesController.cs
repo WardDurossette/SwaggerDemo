063b91681e1dd1e7933e1a3c66037b2cff6451a2
Ward Durossette <ward@twu.edu>
2018-02-16 14:01:56 -0600

    Add project files.

---
+++ cDB/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cDB.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        IHostingEnvironment _env;
+
+
+        public ValuesController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {
+                "MachineName", Environment.MachineName,
+                "EnvironmentName", _env.EnvironmentName 
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ cDB/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace cDB
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+
+            services.AddSwaggerGen(options => 
+                options.SwaggerDoc("v1", new Info { Title = "cDB", Version = "v1"})
+                );
+
+
+            services.Configure<IISOptions>(options =>
+            { 
+                options.ForwardClientCertificate = false;
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+
+            if (env.IsDevelopment() || env.IsStaging())
+            {
+
+                string sSwaggerPath = "/SwaggerDemo/swagger/v1/swagger.json";
+                if (System.Diagnostics.Debugger.IsAttached) sSwaggerPath = "/swagger/v1/swagger.json";
+
+                app.UseSwaggerUI(options =>
+                    options.SwaggerEndpoint(sSwaggerPath, "cDB")
+                    );
+                
+            }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
